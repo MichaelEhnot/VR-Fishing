1f8404885df876ee92d6d36060d061e7b4d14612
MichaelEhnot <43359524+MichaelEhnot@users.noreply.github.com>
2021-02-23 17:57:47 -0500

    working on water barrier

---
+++ Assets/DetectPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = new Vector3(175, 26.25f, 50.31f);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.gameObject.transform.position = new Vector3(175, 26.25f, 50.31f);
+        }
+    }
+}
